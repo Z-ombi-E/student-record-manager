942b3a62293557fe84e7602ba588038fd143d9ee
Nethum <nethumnenula2004@gmail.com>
2026-05-04 19:30:59 +0530

    Implemented case-insensitive searchStudent feature

--- Main.java
+++ Main.java
@@ -8,6 +8,9 @@ public class Main {
         viewStudents();
         deleteStudent("Alice");
         countStudents();
+
+        searchStudent("alice");
+        searchStudent("Charlie");
     }
 
     static void addStudent(String name) {
@@ -29,6 +32,20 @@ public class Main {
 
     static void countStudents() {
         System.out.println("Total students: " + students.size());
+    }
+
+    static void searchStudent(String name){
+        boolean studentFound = false;
+        for(String student : students){
+            if(student.equalsIgnoreCase(name)){
+                System.out.println(student +" found.");
+                studentFound = true;
+                break;
+            }
+        }
+        if(!studentFound){
+            System.out.println(name + " not found! Try another..");
+        }
     }
     
 }
